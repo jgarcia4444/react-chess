d2fe985736b481013b3a7a136cdd24eb342c2bd1
Jake Garcia <bballwiz4@gmail.com>
2023-03-07 15:56:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import {store, persistor} from './redux/store';
+import {
+BrowserRouter as Router,
+Routes,
+Route,
+Link
+} from "react-router-dom";
+
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <div className="App">
-          Hello World
-        </div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
       </PersistGate>
     </Provider>
   );
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Home extends React.Component {
+
+    render() {
+
+        return (
+            <div className="homeContainer">
+                Home Page
+            </div>
+        )
+    }
+}
+
+export default Home;
