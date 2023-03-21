624b92de40879a8524d9e2a26e092bd7dc5681fe
Jake Garcia <bballwiz4@gmail.com>
2023-03-21 15:28:40 -0700

    The player indicator now appears.

--- src/pages/Game/LocalGame/index.js
+++ src/pages/Game/LocalGame/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import PlayerGameColumn from '../../../components/PlayerGameColumn';
 import Container from '../../../shared/Container';
@@ -6,8 +6,19 @@ import GameBoard from '../../../components/GameBoard';
 
 const LocalGame = () => {
 
+    const [playerTurnIndex, setPlayerTurnIndex] = useState(0);
+    const players = ["Player 1", "Player 2"];
+
+    const showPlayersTurn = () => {
+        return `${players[playerTurnIndex]}'s Turn `
+    }
+
+
     return (
         <Container>
+            <div className="w-full mb-12 font-bold text-blue-500 text-3xl">
+                {showPlayersTurn()}
+            </div>
             <div className="flex flex-row w-full justify-evenly">
                 <PlayerGameColumn isPlayerOne={true} />
                 <GameBoard isLocal={true} />
