dd9e2e29680494898bf9bb7c242f81f4448fad16
Jake Garcia <bballwiz4@gmail.com>
2023-03-22 16:38:41 -0700

    Added a function to the AuthSubmitButton that will display a loader when neccessary and connected the component to the redux store so that it has access to the loading property of the userReducer.

--- src/components/Buttons/AuthSubmitButton/index.js
+++ src/components/Buttons/AuthSubmitButton/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {FiLoader} from 'react-icons/fi';
 
-const AuthSubmitButton = ({isLogin, authSubmitClick}) => {
+const AuthSubmitButton = ({isLogin, authSubmitClick, loading}) => {
+
+    const renderButtonDisplay = () => {
+        if (loading === true) {
+            return <FiLoader color={'#fff'} className="animate-spin" size={22}/>
+        } else {
+            return isLogin === true ? "Login" : "Sign Up"
+        }
+    }
 
     return (
-        <button onClick={authSubmitClick} className="w-full my-2 bg-blue-500 py-2 rounded text-white font-bold hover:bg-blue-600">
-            {isLogin === true ? "Login" : "Sign Up"}
+        <button onClick={authSubmitClick} className="w-full my-2 bg-blue-500 py-2 rounded text-white font-bold hover:bg-blue-600 flex flex-row justify-center">
+            {renderButtonDisplay()}
         </button>
     )
 }
 
-export default AuthSubmitButton
+const mapStateToProps = state => {
+    return {
+        loading: state.user.loading,
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    null
+)(AuthSubmitButton);
