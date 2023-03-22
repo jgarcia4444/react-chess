a87fb6cd2aa2354022f075fab922638deab6f64d
Jake Garcia <bballwiz4@gmail.com>
2023-03-22 13:09:27 -0700

    Added in the 3 reducer cases needed to handle creating a user.

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,9 +1,39 @@
 const initialState = {
-    email: "",
+    userInfo: {
+        username: "",
+        email: "",
+        id: "",
+    },
+    errors: [],
+    generalError: "",
+    loading: false
 }
 
 const userReducer = (state=initialState, action) => {
     switch(action.type) {
+        case "USER_CREATION_ERROR":
+            let signUpErrors = action.errors !== undefined ? action.errors : [];
+            return {
+                ...state,
+                loading: false,
+                errors: signUpErrors,
+                generalError: action.message,
+            }
+        case "USER_CREATION_SUCCESS":
+            return {
+                ...state,
+                userInfo: {
+                    ...action.user_info
+                },
+                loading: false,
+                generalError: "",
+                errors: []
+            }
+        case "CREATING_USER":
+            return {
+                ...state,
+                loading: true,
+            }
         default: 
             return {
                 ...initialState,
