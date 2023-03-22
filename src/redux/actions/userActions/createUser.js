ed151887128b5e91e5fd88b06861dcd74a88ec81
Jake Garcia <bballwiz4@gmail.com>
2023-03-22 16:24:52 -0700

    Created the createUser action and gave it its logic. Also created the Url object in the config folder to hold the base url.

---
+++ src/redux/actions/userActions/createUser.js
@@ -0,0 +1,35 @@
+import Urls from "../../../config/Urls";
+const {baseUrl} = Urls;
+
+const createUser = userInfo => {
+    let url = `${baseUrl}/users`;
+    let options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({user_info: userInfo}),
+    }
+    return async dispatch => {
+        dispatch({type: "CREATING_USER"});
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                let {success} = data;
+                if (success === true) {
+
+                } else {
+                    let {error} = data;
+                    let {message} = error;
+                    if (error.errors !== undefined) {
+                        let {errors} = error;
+                        return dispatch({type: "USER_CREATION_ERROR", message, errors});
+                    } else {
+                        return dispatch({type: "USER_CREATION_ERROR", message});
+                    }
+                }
+            })
+    }
+}
+
+export default createUser;
