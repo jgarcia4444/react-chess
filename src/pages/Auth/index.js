8333b044b89c996169c2b4772d4440c3126f7bcb
Jake Garcia <bballwiz4@gmail.com>
2023-03-28 21:52:26 -0700

    Was able to decode the the JWT sent back from google.

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,7 +1,8 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { GoogleLogin } from '@react-oauth/google';
+import { decodeToken } from "react-jwt";
 
 import PageTitle from '../../components/PageTitle';
 import Container from '../../shared/Container';
@@ -13,6 +14,7 @@ const Auth = ({user}) => {
     const location = useLocation();
     const navigate = useNavigate();
     const authState = location.state !== null ? location.state.authState : "Login";
+    const [token, setToken] = useState('');
 
     const {userInfo, errors, generalError} = user;
     const {email, username} = userInfo;
@@ -22,7 +24,8 @@ const Auth = ({user}) => {
     }
 
     const handleGoogleLoginSuccess = (res) => {
-        console.log("Google Login Success", res);
+        const decodedToken = decodeToken(res.credential);
+        console.log("Decoded Token", decodedToken);
     }
 
     const handleGoogleLoginError = (res) => {
@@ -40,16 +43,20 @@ const Auth = ({user}) => {
             <div className=" w-3/4 text-left m-auto">
                 <PageTitle title={authState} />
                 <AuthForm isLogin={configureIsLogin()} />
-                <GoogleLogin
-                    onSuccess={handleGoogleLoginSuccess}
-                    onError={handleGoogleLoginError} 
-                    theme='outline'
-                    size='medium'
-                    text={authState === 'Login' ? "signin_with" : "signup_with"}
-                    shape='pill'
-                    logo_alignment='left'
-                    ux_mode='popup'
-                />
+                <div className="mt-2">
+                    <GoogleLogin
+                        onSuccess={handleGoogleLoginSuccess}
+                        onError={handleGoogleLoginError} 
+                        theme='outline'
+                        size='medium'
+                        text={authState === 'Login' ? "signin_with" : "signup_with"}
+                        shape='rectangular'
+                        logo_alignment='left'
+                        ux_mode='popup'
+                        login_uri='http://localhost:3000/google_auth/callback'
+                        scope="email"
+                    />
+                </div>
                 {/* <OAuthButton isLogin={authState === "Login" ? true : false} isGoogleOAuth={true}/>
                 <OAuthButton isLogin={authState === "Login" ? true : false} isGoogleOAuth={false}/> */}
             </div>
