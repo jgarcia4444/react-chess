4d047f3c8f64eae9709415e29f1bf3c7740035fc
Jake Garcia <bballwiz4@gmail.com>
2023-04-04 19:26:17 -0700

    Created a cipher object that will be used to help with the display of the correct images for pieces.

---
+++ src/components/GameBoard/GamePiece/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GamePiece = () => {
+
+    const gamePieceImg = () => {
+
+    }
+
+    return (
+        <img src={gamePieceImg()} />
+    )
+};
+
+export default GamePiece;
---
+++ src/config/gamePieceImgCipher.js
@@ -0,0 +1,30 @@
+
+import blackPawn from '../media/pieces/bPawn.svg';
+import blackBishop from '../media/pieces/bBishop.svg';
+import blackKnight from '../media/pieces/bKnight.svg';
+import blackRook from '../media/pieces/bRook.svg';
+import blackQueen from '../media/pieces/bQueen.svg';
+import blackKing from '../media/pieces/bKing.svg';
+
+const gamePieceImgCipher = {
+    'pawn': {
+        black: blackPawn
+    },
+    'bishop': {
+        black: blackBishop,
+    },
+    'knight': {
+        black: blackKnight,
+    },
+    'rook': {
+        black: blackRook
+    },
+    'queen': {
+        black: blackQueen
+    },
+    'king': {
+        black: blackKing,
+    }
+};
+
+export default gamePieceImgCipher;
